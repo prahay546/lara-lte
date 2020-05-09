dd1ab637accbb467f5e49fe4bceba2d2711862ff
prahay <pratamanpurwiyatna@gmail.com>
2020-05-09 10:34:39 +0700

    2nd commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    STBA TEST
+                    Lara-LTE
                 </div>
 
                 <div class="links">
